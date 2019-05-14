e81e85300fdd99987cfcbaa9f5faccf735240407
Ciasterix <cwaligora97@gmail.com>
2019-05-14 18:47:44 +0200

    Checking if user exists in db and adding new user to db

--- app/src/main/java/com/example/mieszkanko/MainActivity.java
+++ app/src/main/java/com/example/mieszkanko/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.mieszkanko;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.multidex.MultiDex;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
@@ -13,6 +15,8 @@ import com.example.mieszkanko.BottomNavigationBarFragments.ProfileFragment;
 import com.example.mieszkanko.BottomNavigationBarFragments.ScheduleFragment;
 import com.example.mieszkanko.BottomNavigationBarFragments.ShoppingListFragment;
 import com.example.mieszkanko.BottomNavigationBarFragments.StatisticsFragment;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,10 +26,16 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     public List<String> productsNameToBuyList;
     public List<String> productsDescriptionToBuyList;
 
+    DatabaseReference mRootRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        mRootRef = FirebaseDatabase.getInstance().getReference().child("users");
+
 
         productsNameToBuyList = new ArrayList<>();
         productsNameToBuyList.add("Papier toaletowy");
@@ -88,5 +98,11 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
         imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
     }
+
+//    @Override
+//    protected void attachBaseContext(Context base) {
+//        super.attachBaseContext(base);
+//        MultiDex.install(this);
+//    }
 
 }
