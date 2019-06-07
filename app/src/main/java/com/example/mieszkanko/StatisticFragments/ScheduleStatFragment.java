19907dbc4e7fb4d3a9b004e6f4015ae314355ba6
PBiskup <p.biskup@expansio.pl>
2019-06-07 18:01:30 +0200

    begin schedule stats

--- app/src/main/java/com/example/mieszkanko/StatisticFragments/ScheduleStatFragment.java
+++ app/src/main/java/com/example/mieszkanko/StatisticFragments/ScheduleStatFragment.java
@@ -9,10 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.example.mieszkanko.AccountSettings.AccountSettings;
+import com.example.mieszkanko.Models.Schedule;
+import com.example.mieszkanko.Models.User;
 import com.example.mieszkanko.R;
 
 import java.util.ArrayList;
@@ -24,7 +28,9 @@ public class ScheduleStatFragment extends Fragment {
     private GridView gridView;
     private Spinner periodsSpinner;
 
-    private List<String> tempList = new ArrayList<>();
+    private List<NickAndAmountOfCleanedUp> summarySList = new ArrayList<>();
+
+    private List<String> spinnerList = new ArrayList<>();
     private ArrayAdapter<String> spinnerAdapter;
 
 
@@ -35,14 +41,21 @@ public class ScheduleStatFragment extends Fragment {
         ViewGroup viewRoot = (ViewGroup) inflater.inflate(R.layout.fragment_stat_schedule, null);
 
         periodsSpinner = viewRoot.findViewById(R.id.PeriodsSpinner);
+        gridView = viewRoot.findViewById(R.id.GridViewScheduleStat);
 
-        tempList.add("1");
-        tempList.add("2");
-        tempList.add("3");
-        tempList.add("4");
+        Schedule schedule = new Schedule();
+
+        for (User user : AccountSettings.getApartment().getRoommatesUser()){
+            summarySList.add(new NickAndAmountOfCleanedUp(user.getNick()));
+        }
+
+        for(int i = 0; i < schedule.getRoomsschedule().size(); i++)
+        {
+            spinnerList.add(Integer.toString(i + 1));
+        }
 
 
-        spinnerAdapter = new ArrayAdapter<>(getActivity(), R.layout.spinner_item, tempList);
+        spinnerAdapter = new ArrayAdapter<>(getActivity(), R.layout.spinner_item, spinnerList);
         periodsSpinner.setAdapter(spinnerAdapter);
 
         periodsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
@@ -63,5 +76,55 @@ public class ScheduleStatFragment extends Fragment {
 
 
         return viewRoot;
+    }
+
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View customView = getLayoutInflater().inflate(R.layout.schedule_stat_custom_layout, null);
+
+
+
+            return customView;
+        }
+    }
+
+    class NickAndAmountOfCleanedUp {
+
+        private String nick;
+        private int amount = 0;
+
+        NickAndAmountOfCleanedUp(String nick){
+            this.nick = nick;
+        }
+
+        public String getNick() {
+            return nick;
+        }
+
+        public int getAmount() {
+            return amount;
+        }
+
+        public void addToAmount()
+        {
+            this.amount += 1;
+        }
     }
 }
