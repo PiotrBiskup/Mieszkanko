046b90e692b4e6b22f280af6f3ba574a844476f5
PBiskup <p.biskup@expansio.pl>
2019-06-08 20:29:33 +0200

    final version v1

--- app/src/main/java/com/example/mieszkanko/StatisticFragments/ScheduleStatFragment.java
+++ app/src/main/java/com/example/mieszkanko/StatisticFragments/ScheduleStatFragment.java
@@ -15,6 +15,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.mieszkanko.AccountSettings.AccountSettings;
+import com.example.mieszkanko.Models.Roomsschedule;
 import com.example.mieszkanko.Models.Schedule;
 import com.example.mieszkanko.Models.User;
 import com.example.mieszkanko.R;
@@ -26,9 +27,13 @@ public class ScheduleStatFragment extends Fragment {
 
 
     private GridView gridView;
+    private CustomAdapter customAdapter = new CustomAdapter();
     private Spinner periodsSpinner;
 
+
     private List<NickAndAmountOfCleanedUp> summarySList = new ArrayList<>();
+    private int totalAppearance;
+    private int totalAmounts;
 
     private List<String> spinnerList = new ArrayList<>();
     private ArrayAdapter<String> spinnerAdapter;
@@ -49,7 +54,7 @@ public class ScheduleStatFragment extends Fragment {
             summarySList.add(new NickAndAmountOfCleanedUp(user.getNick()));
         }
 
-        for(int i = 0; i < AccountSettings.getApartment().getRoommatesUser().size(); i++)
+        for(int i = 0; i < AccountSettings.getSchedule().getPeriodList().size() - 1; i++)
         {
             spinnerList.add(Integer.toString(i + 1));
         }
@@ -65,6 +70,9 @@ public class ScheduleStatFragment extends Fragment {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
             {
                 ((TextView) view).setTextColor(getResources().getColor(R.color.colorAccent));
+                countStatistics();
+                customAdapter.notifyDataSetChanged();
+
             }
 
             @Override
@@ -73,7 +81,7 @@ public class ScheduleStatFragment extends Fragment {
             }
         });
 
-
+        gridView.setAdapter(customAdapter);
 
         return viewRoot;
     }
@@ -82,7 +90,7 @@ public class ScheduleStatFragment extends Fragment {
 
         @Override
         public int getCount() {
-            return 0;
+            return summarySList.size() + 1;
         }
 
         @Override
@@ -99,9 +107,97 @@ public class ScheduleStatFragment extends Fragment {
         public View getView(int position, View convertView, ViewGroup parent) {
             View customView = getLayoutInflater().inflate(R.layout.schedule_stat_custom_layout, null);
 
+            TextView nickTV = customView.findViewById(R.id.nickSStatTextView);
+            TextView statTV = customView.findViewById(R.id.ratioSStatTextView);
+
+            if(getCount() == 1)
+            {
+
+                nickTV.setText("No previous periods");
+                statTV.setText("");
+
+            } else {
+
+                Double ratio = 0.0;
+
+                if (position == 0)
+                {
+                    customView.setBackground(getResources().getDrawable(R.drawable.shadow_primary_color));
+                    nickTV.setText("In total");
+                    ratio = Double.valueOf(totalAmounts) / Double.valueOf(totalAppearance);
+                    statTV.setText(Integer.toString(totalAmounts) + " / " + Integer.toString(totalAppearance)
+                            + "  ->  " + String.format ("%.2f", (ratio * 100)) + "%");
+
+                } else {
+
+                    String nick = summarySList.get(position - 1).getNick();
+                    int usr_amounts = summarySList.get(position - 1).getAmount();
+                    int usr_appearance = summarySList.get(position - 1).getApperance();
+                    ratio = Double.valueOf(usr_amounts) / Double.valueOf(usr_appearance);
+
+                    nickTV.setText(nick);
+                    statTV.setText(Integer.toString(usr_amounts) + " / " + Integer.toString(usr_appearance)
+                            + "  ->  " + String.format ("%.2f", (ratio * 100)) + "%");
+
+
+                }
+            }
+
+
 
 
             return customView;
+        }
+    }
+
+    private void countStatistics()
+    {
+        clearUsersAmounts();
+        int tempTotalAppearance = 0;
+        int tempTotalAmounts = 0;
+        List<NickAndAmountOfCleanedUp> tempList = summarySList;
+        int numberOfPeriods = periodsSpinner.getSelectedItemPosition() + 1;
+
+        for (int i = AccountSettings.getSchedule().getPeriodList().size() - 2;
+             i >= AccountSettings.getSchedule().getPeriodList().size() - (numberOfPeriods + 1);
+             i--)
+        {
+
+            for(Roomsschedule room : AccountSettings.getSchedule().getPeriodList().get(i).getRoomsschedule())
+            {
+
+                String room_user_nick = AccountSettings.getApartment().findUserByKey(room.getUser());
+
+                for (NickAndAmountOfCleanedUp user : tempList){
+
+                    if(user.getNick().equals(room_user_nick))
+                    {
+                        user.addToApperance();
+                        tempTotalAppearance += 1;
+
+                        if(room.getStatus())
+                        {
+                            user.addToAmount();
+                            tempTotalAmounts += 1;
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        summarySList = tempList;
+        totalAmounts = tempTotalAmounts;
+        totalAppearance = tempTotalAppearance;
+
+    }
+
+    private void clearUsersAmounts(){
+
+        for (NickAndAmountOfCleanedUp x: summarySList){
+            x.clearAmount();
         }
     }
 
@@ -109,6 +205,7 @@ public class ScheduleStatFragment extends Fragment {
 
         private String nick;
         private int amount = 0;
+        private int appearance = 0;
 
         NickAndAmountOfCleanedUp(String nick){
             this.nick = nick;
@@ -122,9 +219,22 @@ public class ScheduleStatFragment extends Fragment {
             return amount;
         }
 
+        public int getApperance() {
+            return appearance;
+        }
+
         public void addToAmount()
         {
             this.amount += 1;
+        }
+
+        public void addToApperance(){
+            this.appearance += 1;
+        }
+
+        public void clearAmount(){
+            this.amount = 0;
+            this.appearance = 0;
         }
     }
 }
